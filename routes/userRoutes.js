f90a23e8f70a964d6308bbb75c4224a667ee5f61
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-03-18 17:00:43 +0200

    fix error

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createUser } from "../controller.js";
-import inputValidation from "../controller.js";
+import inputValidation from "../stripeController.js";
 
 const router = express.Router();
 
